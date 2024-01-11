d410da1bc601a18e7e3a7018aa5e07497c36ffe8
bouaik9 <ayoub.bouaik.22@gmail.com>
2024-01-11 15:47:24 +0100

    host

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-v+&3#b7^tv@@dkfawnc$q^m#j+flwpy9ww-kf+%9b6-hiy)!kh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
